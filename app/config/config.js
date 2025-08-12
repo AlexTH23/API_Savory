a22ce0c0f70d7d2e3bd7a3a4bc2b85c16bacedbb
FerHG0 <2236001342@utna.edu.mx>
2025-08-12 00:13:38 -0600

    configuracion a mongo atlas, direccion a la bd

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: process.env.MONGO_URL || process.env.DB || 'mongodb://localhost:27017/restaurante_savory'
+    DB: process.env.MONGO_URI || process.env.DB || 'mongodb://localhost:27017/restaurante_savory'
 };

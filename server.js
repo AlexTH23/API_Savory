f2b7a36712f846b07c51db1eb18cda5153a2df53
FerHG0 <2236001342@utna.edu.mx>
2025-08-12 01:19:19 -0600

    correcion en server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const app = require('./app/app')
 const config = require('./app/config/config')
 const conexion = require('./app/config/conexion')
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 conexion.connect()
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.listen(config.PORT, '0.0.0.0', () =>{
-    console.log(`Aplicacion corriendo en puerto ${config.PORT}`);
-})
+  .then(() => {
+    app.listen(config.PORT, '0.0.0.0', () => {
+      console.log(`Aplicacion corriendo en puerto ${config.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.error('No se pudo conectar a la base de datos:', err)
+  })
